b40c54e6c21bcdfada0c3e9563767570e38ee8a5
tengfei-emory <tfei@emory.edu>
2019-02-18 16:25:30 -0500

    CRAN

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -11,9 +11,8 @@
 #' @param idx Random selected columns input from the main algorithm.
 #' @author Teng Fei. Email: tfei@emory.edu
 #' @export
-#' @useDynLib scBatch, .registration = TRUE
 derif <- function(c, w, d, core, idx) {
-    .Call(`_scBatch_derif`, c, w, d, core, idx)
+    .Call('_scBatch_derif', PACKAGE = 'scBatch', c, w, d, core, idx)
 }
 
 #' Correct RNA-seq count matrix subject to batch effects by sample distance matrix correction (faster Rcpp version)
@@ -32,8 +31,7 @@ derif <- function(c, w, d, core, idx) {
 #' @author Teng Fei. Email: tfei@emory.edu
 #' @references Fei et al (2018), Mitigating the adverse impact of batch effects in sample pattern detection, Bioinformatics, <https://doi.org/10.1093/bioinformatics/bty117>.
 #' @export
-#' @useDynLib scBatch, .registration = TRUE
 scBatchCpp <- function(c, w, d, m, max, step, tol, derif, verbose) {
-    .Call(`_scBatch_scBatchCpp`, c, w, d, m, max, step, tol, derif, verbose)
+    .Call('_scBatch_scBatchCpp', PACKAGE = 'scBatch', c, w, d, m, max, step, tol, derif, verbose)
 }
 
